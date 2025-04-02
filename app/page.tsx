ff93164933c5d91af0db82efe841cbb037a7adef
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2025-04-03 02:57:42 +0500

    images section done chapter  3 done

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,11 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from "./home.module.css"
 import { lusitana } from './ui/fonts';
+import Image from 'next/image';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <p className={lusitana.className}>this is lusitana</p>
+      {/* <p className={lusitana.className}>this is lusitana</p> */}
       {/* testing tailwind css  */}
       {/* <h1 className='text-blue-500'>i am blue</h1> */}
       {/* <h2 className='t'>how to you</h2> */}
@@ -14,7 +15,7 @@ export default function Page() {
   className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
 /> */}
 
-<div className={styles.shape}></div>
+{/* <div className={styles.shape}></div> */}
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         <AcmeLogo />
       </div>
@@ -36,6 +37,20 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image
+        src="/hero-desktop.png"
+        width={1000}
+        height={760}
+        className="hidden md:block"
+        alt="Screenshots of the dashboard project showing desktop version"
+      />
+          <Image
+        src="/hero-mobile.png"
+        width={560}
+        height={620}
+        className="block md:hidden"
+        alt="Screenshots of the dashboard project showing desktop version"
+      />
         </div>
       </div>
     </main>
