47fcb8262038f75329a5300a4200b9c52a1e3a36
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2025-04-03 02:54:46 +0500

    adding fonts successfully

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,11 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from "./home.module.css"
-
+import { lusitana } from './ui/fonts';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
+      <p className={lusitana.className}>this is lusitana</p>
       {/* testing tailwind css  */}
       {/* <h1 className='text-blue-500'>i am blue</h1> */}
       {/* <h2 className='t'>how to you</h2> */}
@@ -15,7 +16,7 @@ export default function Page() {
 
 <div className={styles.shape}></div>
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* <AcmeLogo /> */}
+        <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
