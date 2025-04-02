d95563cd2d079f771f9e857d095bf577c140bd94
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2025-04-03 02:47:02 +0500

    chapter 2 done

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,19 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import styles from "./home.module.css"
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       {/* testing tailwind css  */}
-      <h1 className='text-blue-500'>i am blue</h1>
+      {/* <h1 className='text-blue-500'>i am blue</h1> */}
+      {/* <h2 className='t'>how to you</h2> */}
+      {/* <div
+  className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
+/> */}
+
+<div className={styles.shape}></div>
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         {/* <AcmeLogo /> */}
       </div>
